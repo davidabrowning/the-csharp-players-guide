1fdf0e23c75491c4b75882fe2a62dabf98517318
David Browning <david.a.browning@gmail.com>
2025-03-04 15:15:02 +0100

    Initial setup.

---
+++ Chapter24BossTicTacToe/Chapter24BossTicTacToe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Chapter24BossTicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestRunner testRunner = new TestRunner();
+            testRunner.RunTests();
+        }
+    }
+}
---
+++ Chapter24BossTicTacToe/Chapter24BossTicTacToe/TestRunner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter24BossTicTacToe
+{
+    internal class TestRunner
+    {
+        public void RunTests()
+        {
+
+        }
+    }
+}
