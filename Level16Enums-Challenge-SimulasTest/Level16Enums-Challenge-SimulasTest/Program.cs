f3b08268801e81658308d7986a2f7afb9aa59ecb
David Browning <david.a.browning@gmail.com>
2025-03-09 15:33:09 +0100

    Update folder and solution names.

---
+++ Level16Enums-Challenge-SimulasTest/Level16Enums-Challenge-SimulasTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Level16Enums_Challenge_SimulasTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
